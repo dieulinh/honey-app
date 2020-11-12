a411c3a8972818473d4a8f41185be760ff6a1276
Linh Nguyen <linhvn09@gmail.com>
2020-11-12 09:19:41 +0700

    fix url api

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
-process.env.ROOT_API = 'http://www.i-class.info/api'
+process.env.ROOT_API = 'http://www.matongcamau.com/api'
 const environment = require('./environment')
 
 
